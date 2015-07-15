083ed9d821ad68728cffe8705ea98de21a6a1b67
JeremyPacker <Jpacker@siu.edu>
2015-07-15 16:42:42 -0500

    Final Upload

--- app/src/main/java/app/com/socialserve/EventAdapter.java
+++ app/src/main/java/app/com/socialserve/EventAdapter.java
@@ -23,42 +23,10 @@ public class EventAdapter extends ParseQueryAdapter<Event>  {
         });
     }
 
-
-    //query to get events with the specified name
-    public EventAdapter(Context context, final String name, final String obj, int flag1) {
-        super(context, new ParseQueryAdapter.QueryFactory<Event>() {
-            public ParseQuery create() {
-                ParseQuery query = new ParseQuery("events");
-
-                if(!name.isEmpty()){
-                    query.whereEqualTo(name, obj);
-                }
-                return query;
-            }
-        });
-        flag = flag1;
-    }
-
     // Customize the layout by overriding getItemView
    // @Override
     public View getItemView(Event event, View v, ViewGroup parent) {
 
-        if(flag == 1) {
-            v = View.inflate(getContext(), R.layout.my_events_row, null);
-            super.getItemView(event, v, parent);
-
-            // Add all data to the layout
-            TextView titleTextView = (TextView) v.findViewById(R.id.list_name);
-            titleTextView.setText(event.getTitle());
-
-            TextView date = (TextView) v.findViewById(R.id.list_date);
-            date.setText("on " + event.getTime());
-
-            TextView attending = (TextView) v.findViewById(R.id.eventAttending);
-            attending.setText( " " + event.getSeatsSold() + " / " + event.getSeats());
-
-        }
-        else {
             v = View.inflate(getContext(), R.layout.home_row, null);
             super.getItemView(event, v, parent);
 
